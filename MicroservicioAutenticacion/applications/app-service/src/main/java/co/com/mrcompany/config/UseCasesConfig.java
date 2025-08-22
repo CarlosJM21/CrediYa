098cef7b1e9250318e0a37d948a814947904a4eb
CarlosJM21 <lee21_8@hotmail.com>
2025-08-22 17:50:42 -0500

    chore: se crea la estructura para la HU1 con el scaffold.

---
+++ MicroservicioAutenticacion/applications/app-service/src/main/java/co/com/mrcompany/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.mrcompany.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.mrcompany.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
