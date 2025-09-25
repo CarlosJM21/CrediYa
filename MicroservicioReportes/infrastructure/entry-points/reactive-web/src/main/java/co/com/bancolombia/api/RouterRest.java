b686ad1879c56b31a24e16d1f0bee740c63ee417
CarlosJM21 <lee21_8@hotmail.com>
2025-09-25 18:56:20 -0500

    Feat:  se agregan documentacion de Open Api

--- MicroservicioReportes/infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
+++ MicroservicioReportes/infrastructure/entry-points/reactive-web/src/main/java/co/com/bancolombia/api/RouterRest.java
@@ -1,7 +1,16 @@
 package co.com.bancolombia.api;
 
+import co.com.bancolombia.model.Report;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springdoc.core.annotations.RouterOperation;
+import org.springdoc.core.annotations.RouterOperations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
@@ -12,6 +21,15 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class RouterRest {
     @Bean
+    @RouterOperations({
+            @RouterOperation( path = "/api/report",
+                    produces = { MediaType.APPLICATION_JSON_VALUE }, method = RequestMethod.GET, beanClass = Handler.class, beanMethod = "Report",
+                    operation = @Operation( operationId = "Report",
+                            responses = { @ApiResponse(responseCode = "200", description = "Get Report.",
+                                    content = @Content(schema = @Schema(implementation = Report.class)))}
+                    )
+            )
+    })
     public RouterFunction<ServerResponse> routerFunction(Handler handler) {
         return route(GET("/api/report"), handler::Report);
     }
